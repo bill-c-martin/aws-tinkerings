c81abac396effa1a90cebeaf4a35deb6e81858a5
Bill Martin <bill.m@globalwidget.com>
2020-02-20 22:19:35 -0500

    Add catch

--- test-lambda-1/promise-demo.js
+++ test-lambda-1/promise-demo.js
@@ -1,10 +1,11 @@
 let promise = new Promise((resolve, reject) => {
     // do a thing, possibly async, then…
-    const thingsAreFine = /* do some work and capture the result */ true;
+    const thingsAreFine = /* do some work and capture the result */ false;
     if (thingsAreFine) {
         resolve("Stuff worked!");
     } else {
         reject(Error("It broke"));
     }
 })
-    .then(console.log);
+    .then(console.log)
+    .catch(err => console.log(`Errors: ${err}`));
