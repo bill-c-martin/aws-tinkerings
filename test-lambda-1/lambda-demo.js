51b11866d2e1288293af58aa723986b1a3f037a0
Bill Martin <bill.m@globalwidget.com>
2020-02-20 22:43:44 -0500

    Add name to test cases

--- test-lambda-1/lambda-demo.js
+++ test-lambda-1/lambda-demo.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     return new Promise((resolve, reject) => {
-        console.log(`hello lambda`);
+        console.log(`hello ${event.name}`);
         resolve();
     })
 };
